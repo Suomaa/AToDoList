fb0e58d34077d15d5352f6195fb5615a6f0b2b4d
Suomaa <a.antomonov@mail.ru>
2017-09-04 17:58:00 +0300

    Add updateUI method

--- app/src/main/java/fi/suomaafrontieroy/atodolist/MainActivity.java
+++ app/src/main/java/fi/suomaafrontieroy/atodolist/MainActivity.java
@@ -13,9 +13,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.content.ContentValues;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import fi.suomaafrontieroy.atodolist.db.TaskContract;
 import fi.suomaafrontieroy.atodolist.db.TaskDbHelper;
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private TaskDbHelper mHelper;
     private ListView mTaskListView;
+    private ArrayAdapter<String> mAdapter;
 
 
     @Override
@@ -44,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         }
         cursor.close();
         db.close();
+
+        updateUI();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
                                         values,
                                         SQLiteDatabase.CONFLICT_REPLACE);
                                 db.close();
+                                updateUI();
                             }
                         })
                         .setNegativeButton("Cancel", null)
@@ -100,5 +107,27 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE, new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE}, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this, R.layout.item_todo, R.id.task_title, taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
     }
 }
