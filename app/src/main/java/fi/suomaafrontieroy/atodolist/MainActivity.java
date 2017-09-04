ddf7cb017ca07e116c9aa65133b2292bda036012
Suomaa <a.antomonov@mail.ru>
2017-09-04 18:51:29 +0300

    Del unused import link

--- app/src/main/java/fi/suomaafrontieroy/atodolist/MainActivity.java
+++ app/src/main/java/fi/suomaafrontieroy/atodolist/MainActivity.java
@@ -3,7 +3,6 @@ package fi.suomaafrontieroy.atodolist;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
