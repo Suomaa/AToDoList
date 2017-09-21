2adbe89d0b54b8c1676c562da5b3ef1fd424a79c
Suomaa <a.antomonov@mail.ru>
2017-09-21 19:01:39 +0300

    Change About info

--- app/src/main/java/fi/suomaafrontieroy/atodolist/AboutActivity.java
+++ app/src/main/java/fi/suomaafrontieroy/atodolist/AboutActivity.java
@@ -1,7 +1,11 @@
 package fi.suomaafrontieroy.atodolist;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,6 +13,20 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getVersionInfo();
+    }
+
+    private void getVersionInfo() {
+        String version = "";
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version= packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextView textViewVersionInfo = (TextView) findViewById(R.id.version);
+        textViewVersionInfo.setText(String.format("Version: %s \n", version));
     }
 
 }
